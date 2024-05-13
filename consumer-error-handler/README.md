71699335c11b4d7c1b29a8273fbaca53a697018e
malexj <alexmaximovj@gmail.com>
2024-05-13 17:38:50 +0300

    Add mock module

---
+++ consumer-error-handler/README.md
@@ -0,0 +1,5 @@
+## Kafka simple error handler
+
+* tutorial:  https://www.youtube.com/watch?v=OnZ7JArSoiU
+* link: https://github.com/xepcules/kafka-consumer-strategies
+
---
+++ consumer-error-handler/src/main/java/com/malex/ConsumerErrorHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.malex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerErrorHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerErrorHandlerApplication.class, args);
+	}
+
+}
